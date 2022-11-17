e25de47c44707a937820b1fed429fa1932af5518
stevans011 <stevanoffs@gmail.com>
2022-11-17 19:14:03 +1100

    Adding content to app.js

--- class-app/src/App.js
+++ class-app/src/App.js
@@ -4,32 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar bg-light">
+       <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+       </div>
+      </nav>
       
-      <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-      
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <h3>Column 1</h3>
+          </div>
+          <div className="col">
+            <h3>Column 2</h3>
+          </div>
+          <div className="col">
+            <h3>Column 3</h3>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div className="container-fluid">
+          <p>Footer</p>
+        </div>
+      </footer>
+
     </div>
   );
 }
