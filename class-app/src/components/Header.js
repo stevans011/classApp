a1a974b96483b21542654310e965decc811421c6
stevans011 <stevanoffs@gmail.com>
2022-11-17 20:15:07 +1100

    added navigation component

--- class-app/src/components/Header.js
+++ class-app/src/components/Header.js
@@ -1,12 +1,17 @@
+import { Navigation } from "./Navigation"
+
 export function Header(props){
 
     return (
 
-        <nav className="navbar bg-light">
+        <header className="navbar bg-light navbar-expand-lg">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">{props.title}</a>
+                <div className="collapse navbar-collapse">
+                    <Navigation />
+                </div>
              </div>
-       </nav>
+       </header>
 
 
     )
