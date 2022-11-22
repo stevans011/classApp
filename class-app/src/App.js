f45cf3b1cee746b39577fbe92726c41636e541ed
stevans011 <stevanoffs@gmail.com>
2022-11-23 00:28:16 +1100

    adding NavData to App.js as data source

--- class-app/src/App.js
+++ class-app/src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {Header} from './components/Header'
 import {Footer} from './components/Footer'
-import {Home} from './pages/Home'
-import {About} from './pages/About'
-import {Contact} from './pages/Contact'
+
+
+const NavData = [
+  { name: "Home", path:"/", public:true},
+  { name: "About", path:"/about", public:true},
+  { name: "Contact", path:"/contact", public:true}
+]
 
 function App() {
   return (
     <div className="App">
 
-      <Header title="My app"/>
+      <Header title="My app" headernav={NavData} />
       
       <div className="container-fluid">
         <div className="row">
--- class-app/src/components/Navigation.js
+++ class-app/src/components/Navigation.js
@@ -1,14 +1,36 @@
+import {useState,useEffect} from 'react'
+
 export function Navigation (props){
+    const [navItems, setNavItems ] = useState([])
 
-    return (
+    useEffect ( () => {
+        if(navItems.length == 0) {
+            setNavItems(props.items )
+        }
+    }, [props.items])
 
-        <div className="navbar-nav me-auto mb 2 lg-0">
-            <a href="#" className="nav-link">Home</a>
-            <a href="#" className="nav-link">About</a>
-            <a href="#" className="nav-link">Contact</a>
-        </div>
+    if (navItems.length > 0) {
+
+        const NavLinks = navItems.map( (item) => {
+            return(
+                <a href={item.path} className="nav-link">{ item.name }</a>
+            )
+         })
+        return (
+
+            <nav className="navbar-nav me-auto mb-2 lg-0">
+                { NavLinks }
+            </nav>
+        )
+    }
+    else{
+        return null
+
+    }
+
+    
 
 
-    )
+    
 
 }
